e6943c515cd0e340801ff114a9ae84370ebed3d5
DmIrina <dmytriieva.iryna@gmail.com>
2021-11-03 20:04:14 +0300

    methods about pheromone, class Ant

--- src/Ant.java
+++ src/Ant.java
@@ -1,18 +1,29 @@
+import java.util.HashMap;
 import java.util.HashSet;
 import java.util.LinkedList;
+import java.util.Map;
 
 public class Ant {
     private int currentCityIndex;
     private boolean elite = false;
-    public LinkedList<Integer> route = new LinkedList<>();
-    public HashSet<Integer> visited = new HashSet<>();
-    public Integer routeLength = 0;
+    private LinkedList<Integer> route = new LinkedList<>();
+    private HashSet<Integer> visited = new HashSet<>();
+    private Integer routeLength = 0;
 
-    public void Run(){
-
+    public void run() {
+        while (visited.size() != Main.CITIES_COUNT){
+            int d = getDestinationIndex();
+            if (d == -1) {
+                System.out.println("Incorrect index");
+            }
+            route.add(d);
+            visited.add(d);
+            routeLength += Main.cityGraph.get(currentCityIndex).get(d);
+            currentCityIndex = d;
+        }
     }
 
-    public boolean isElite(){
+    public boolean isElite() {
         return elite;
     }
 
@@ -22,5 +33,36 @@ public class Ant {
 
     public void setCurrentCityIndex(int currentCityIndex) {
         this.currentCityIndex = currentCityIndex;
+        route.add(currentCityIndex);
+    }
+
+    public Integer getRouteLength() {
+        return routeLength;
+    }
+
+    public LinkedList<Integer> getRoute() {
+        return route;
+    }
+
+    private Integer getDestinationIndex() {
+        HashMap<Double, Integer> probabilities = new HashMap<>();
+        double xSum = 0;
+        for (int j = 0; j < Main.CITIES_COUNT; j++) {
+            if (!visited.contains(j)) {
+                double x = Math.pow(Main.pheromoneMatrix.get(currentCityIndex).get(j), Main.ALFA) * Math.pow(Main.sightMatrix.get(currentCityIndex).get(j), Main.BETA);
+                xSum += x;
+                probabilities.put(xSum, j);
+            }
+        }
+        double rand = Math.random();
+
+        for (Map.Entry<Double, Integer> entry : probabilities.entrySet()) {
+            double probability = entry.getKey() / xSum;
+            Integer destinationIndex = entry.getValue();
+            if (rand < probability) {
+                return destinationIndex;
+            }
+        }
+        return -1;
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,18 +1,22 @@
 import java.util.ArrayList;
+import java.util.LinkedList;
 
 public class Main {
 
     public final static int MIN_VALUE = 5;
     public final static int MAX_VALUE = 150;
     public final static int CITIES_COUNT = 300;
+    public static double LMin = MAX_VALUE * CITIES_COUNT;
+    public final static int ANT_NUMBER = 1000;
+    public static ArrayList<Ant> antsColony;
+    public static int tMax = 100;
+    public final static int ALFA = 1;
+    public final static int BETA = 1;
+    public final static double RO = 0.1;
+    public final static double TAU_0 = 0.01;
     public static ArrayList<ArrayList<Integer>> cityGraph;
     public static ArrayList<ArrayList<Double>> sightMatrix;
     public static ArrayList<ArrayList<Double>> pheromoneMatrix;
-    public static int LMin = MAX_VALUE * CITIES_COUNT;
-    public static final double TAU_0 = 0.01;
-    public static final int ANT_NUMBER = 1000;
-    public static ArrayList<Ant> antsColony;
-    public static int tMax = 100;
 
     public static void generateGraph() {
         ArrayList<ArrayList<Integer>> graphRow = new ArrayList<>();
@@ -31,7 +35,7 @@ public class Main {
         cityGraph = graphRow;
     }
 
-    public static void initSightMatrix(){
+    public static void initSightMatrix() {
         ArrayList<ArrayList<Double>> matrixRow = new ArrayList<>();
         ArrayList<Double> matrixCol;
         for (int i = 0; i < CITIES_COUNT; i++) {
@@ -41,14 +45,14 @@ public class Main {
                 if (i == j) {
                     matrixCol.add(0.0);
                 } else {
-                    matrixCol.add(1.0/cityGraph.get(i).get(j));
+                    matrixCol.add(1.0 / cityGraph.get(i).get(j));
                 }
             }
         }
         sightMatrix = matrixRow;
     }
 
-    public static void initPheromoneMatrix(){
+    public static void initPheromoneMatrix() {
         ArrayList<ArrayList<Double>> matrixRow = new ArrayList<>();
         ArrayList<Double> matrixCol;
         for (int i = 0; i < CITIES_COUNT; i++) {
@@ -65,12 +69,12 @@ public class Main {
         pheromoneMatrix = matrixRow;
     }
 
-    public static void initAntsColony(){
+    public static void initAntsColony() {
         antsColony = new ArrayList<>();
         Ant ant;
         int cityIndex = 0;
-        for (int i = 0; i < ANT_NUMBER; i++){
-            if (cityIndex > CITIES_COUNT){
+        for (int i = 0; i < ANT_NUMBER; i++) {
+            if (cityIndex > CITIES_COUNT) {
                 cityIndex = 0;
             }
             ant = new Ant();
@@ -80,10 +84,52 @@ public class Main {
         }
     }
 
+    public static void updatePheromone() {
+        double deltaTau;
+        int origin;
+        int destination;
+        for (Ant ant : antsColony) {
+            deltaTau = Main.LMin / ant.getRouteLength();
+            LinkedList<Integer> route = ant.getRoute();
+            origin = route.pollFirst();
+            for (Integer city : route) {
+                destination = city;
+                double tau = pheromoneMatrix.get(origin).get(destination);
+                pheromoneMatrix.get(origin).set(destination, tau + deltaTau);
+                origin = destination;
+            }
+        }
+    }
+
+    private static void evaporatePheromone() {
+        for (int i = 0; i < CITIES_COUNT; i++) {
+            for (int j = 0; j < CITIES_COUNT; j++) {
+                double d = Main.pheromoneMatrix.get(i).get(j);
+                Main.pheromoneMatrix.get(i).set(j, d * (1 - Main.RO));
+            }
+        }
+    }
+
     public static void main(String[] args) {
         System.out.println("lab 4");
         Main.generateGraph();
         Main.initSightMatrix();
+        Main.initPheromoneMatrix();
+        Main.initAntsColony();
+        int bestRouteLength = Integer.MAX_VALUE;
+        LinkedList<Integer> bestRoute;
+        for (Ant ant : antsColony) {
+            ant.run();
+            if (ant.getRouteLength() < bestRouteLength) {
+                bestRoute = ant.getRoute();
+                bestRouteLength = ant.getRouteLength();
+                Main.LMin = bestRouteLength;
+            }
+        }
+        Main.evaporatePheromone();
+        Main.updatePheromone();
+
+
         int k = 1;
     }
 }
